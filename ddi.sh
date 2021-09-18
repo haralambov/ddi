5b0521c75cdeeb3c9930decf50d488ad3d86bc03
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-09-18 16:55:10 +0300

    Adds psmisc to the programs list, which gives the 'killall' command

--- ddi.sh
+++ ddi.sh
@@ -153,6 +153,7 @@ function install_programs() {
         "redshift" "unzip" "arandr" "mlocate" "firefox-esr"
         "keepassxc" "tree" "mpv" "xautolock" "nodejs" "npm"
         "python3-pip" "libreoffice" "python3-pip" "fuse"
+        "psmisc"
     )
 
     for PROGRAM in "${PROGRAMS[@]}"; do
