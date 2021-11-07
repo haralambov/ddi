6903f7992686fe4a1d07803cbfa090fc6ec52b12
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-11-07 18:31:14 +0200

    Adds libnotify library(notify-send)

--- ddi.sh
+++ ddi.sh
@@ -170,6 +170,7 @@ function install_programs() {
         "keepassxc" "tree" "mpv" "xautolock" "nodejs" "npm"
         "python3-pip" "libreoffice" "python3-pip" "fuse"
         "psmisc" "compton" "network-manager-gnome" "pavucontrol"
+        "libnotify-bin"
     )
 
     for PROGRAM in "${PROGRAMS[@]}"; do
