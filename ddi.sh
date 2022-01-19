4f3d6c755f7523185e4ed43ae04dfd5d29293d52
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-01-19 20:57:41 +0200

    Removes duplicating package

--- ddi.sh
+++ ddi.sh
@@ -179,7 +179,7 @@ function install_programs() {
         "screenfetch" "htop" "curl" "tlp" "ufw" "lm-sensors"
         "redshift" "unzip" "unrar" "arandr" "mlocate" "firefox-esr"
         "tree" "mpv" "xautolock" "nodejs" "npm"
-        "python3-pip" "libreoffice" "python3-pip" "fuse"
+        "libreoffice" "python3-pip" "fuse"
         "psmisc" "compton" "network-manager-gnome" "pavucontrol"
         "libnotify-bin" "dunst" "brightnessctl" "xclip" "zathura"
         "xsel" "libxfixes-dev" "pass" "pass-extension-otp" "nnn"
